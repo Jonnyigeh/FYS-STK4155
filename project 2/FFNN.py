Fixed some minor mistakes in the linear algebra for the backprop

project 2/FFNN.py
@@ -20,6 +20,7 @@ class NeuralNetwork():
             batch_size=10,
             eta=0.1,
             lmbd=0.0,
+            hidden_act_func="sigmoid",
             classifier=False):
 
         self.X_data_full = X_data
@@ -27,11 +28,14 @@ class NeuralNetwork():
 
         self.n_inputs = X_data.shape[0]
         self.n_features = X_data.shape[1]
+
         self.n_hidden_neurons = n_hidden_neurons
-        if classifier:
+        self.hidden_act_func = hidden_act_func
+        self.classifier = classifier
+        if self.classifier:
             self.n_outputs = n_categories
         else:
-            self.n_outputs = self.n_inputs
+            self.n_outputs = 1
         self.n_hidden_layers = n_hidden_layers
 
         self.epochs = epochs
@@ -52,9 +56,14 @@ class NeuralNetwork():
         input_layer = Layer(self.n_features, self.n_hidden_neurons, input_layer=True)
         self.layers.append(input_layer)
         for l in range(self.n_hidden_layers):
-            self.layers.append(Layer(self.n_hidden_neurons, self.n_hidden_neurons))
+            self.layers.append(Layer(self.n_hidden_neurons, self.n_hidden_neurons,
+                                            act_func=self.hidden_act_func))
 
-        output_layer = Layer(self.n_hidden_neurons, self.n_outputs, output_layer=True)
+        if self.classifier:
+            output_layer = Layer(self.n_hidden_neurons, self.n_outputs, output_layer=True, act_func="sigmoid")
+        else:
+            output_layer = Layer(self.n_hidden_neurons, self.n_outputs, output_layer=True, act_func="linear")
+
         self.layers.append(output_layer)
 
 
@@ -68,10 +77,12 @@ class NeuralNetwork():
             output_prev = output
         # And through output layer
         output_layer = self.layers[-1]
-        self.actual_output = output_layer.forward_prop(output_prev)
-        if classifier:
-            self.actual_output[self.actual_output<0.5] = 0
-            self.actual_output[self.actual_output>=0.5] = 1
+        self.output = output_layer.forward_prop(output_prev)
+
+        if self.classifier:
+            self.output[self.output<0.5] = 0
+            self.output[self.output>=0.5] = 1
+
 
     def feed_forward_out(self, X, Y):
         input_layer = self.layers[0]
@@ -84,10 +95,10 @@ class NeuralNetwork():
 
         # And through output layer
         output_layer = self.layers[-1]
-        if classifier:
-            actual_output[actual_output<0.5] = 0
-            actual_output[actual_output>=0.5] = 1
-            error_estimate = accuracy_score(Y, actual_output)
+        if self.classifier:
+            # actual_output[actual_output<0.5] = 0
+            # actual_output[actual_output>=0.5] = 1
+            error_estimate = accuracy_score(Y.reshape(actual_output.shape), actual_output)
         else:
             actual_output = output_layer.forward_prop(output_prev)
             MSE = mean_squared_error(Y, actual_output)
@@ -98,21 +109,31 @@ class NeuralNetwork():
 
 
     def backpropagation(self):
-        if classifier:
-            output_error = (self.actual_output - self.Y_data)
+        if self.classifier:
+            breakpoint()
+            output_error = (self.output - self.Y_data.reshape(self.output.shape))
         else:
-            output_error = (self.actual_output - self.Y_data) / self.Y_data.size
+            output_error = (self.output - self.Y_data.reshape(self.output.shape)) / self.Y_data.size
+
         output_layer = self.layers[-1]
         error_prev = output_layer.backward_prop(output_error, self.lmbd)
-
+        weights_prev_layer = output_layer.weights
         # Now backpropagate through all the hidden layers
         for l in range(self.n_hidden_layers)[::-1]:
             hidden_layer = self.layers[l+1]
-            error_hidden = hidden_layer.backward_prop(error_prev, self.lmbd)
+            error_hidden = hidden_layer.backward_prop(error_prev, self.lmbd, weights_prev_layer=weights_prev_layer)
+            weights_prev_layer = hidden_layer.weights
             error_prev = error_hidden
+
+
+        input_layer = self.layers[0]
+        error_input = input_layer.backward_prop(error_prev, self.lmbd, weights_prev_layer=weights_prev_layer)
 
         # Now we have all the gradients evaluated in each Layer class instance
         # Lets optimize using our GD method of choice (plain gd in this case)
+        input_layer = self.layers[0]
+        input_layer.weights -= self.eta * input_layer.grad_w
+        input_layer.bias -= self.eta * input_layer.grad_b
 
         for l in range(self.n_hidden_layers):
             hidden_layer = self.layers[l+1]
@@ -127,23 +148,29 @@ class NeuralNetwork():
 
         for i in range(self.epochs):
             print(f"Training... epoch number: {i}")
-            for j in range(self.iterations):
+            for j in range(self.iterations+1):
                 # pick datapoints with replacement
-                chosen_datapoints = np.random.choice(
-                    data_indices, size=self.batch_size, replace=False
-                )
+                # chosen_datapoints = np.random.choice(
+                #     data_indices, size=self.batch_size, replace=False
+                # )
 
                 # minibatch training data
-                self.X_data = self.X_data_full[chosen_datapoints]
-                self.Y_data = self.Y_data_full[chosen_datapoints]
-
+                # self.X_data = self.X_data_full[chosen_datapoints]
+                # self.Y_data = self.Y_data_full[chosen_datapoints]
+                # breakpoint()
+                self.X_data = self.X_data_full
+                self.Y_data = self.Y_data_full
                 self.feed_forward()
                 self.backpropagation()
-            if not classifier:
-                # Calculates the MSE after each epoch, so we can plot and see hwo this changes throughout the training of the NN.
-                self.MSE_train.append(mean_squared_error(self.Y_data, self.actual_output))
+                
 
-        if not classifier:
+            if self.classifier:
+                print("Accuracy score: " + str(accuracy_score(self.output, self.Y_data.reshape(self.output.shape))))
+            else:
+                # Calculates the MSE after each epoch, so we can plot and see hwo this changes throughout the training of the NN.
+                self.MSE_train.append(mean_squared_error(self.Y_data, self.output))
+
+        if not self.classifier:
             # Now we visualize how the MSE changes throughout the epochs
             plt.plot(np.arange(0, self.epochs), self.MSE_train)
             plt.title("MSE as a function of # epochs")
@@ -165,13 +192,14 @@ if __name__  == "__main__":
     x = np.linspace(0,1,n_datapoints)
     y = f(x) + 0.5 * np.random.randn(len(x))
     y = y.reshape(len(y), 1)
-    deg_fit = 5
+    y = (y - np.min(y)) / (np.max(y) - np.min(y))           # min max scaling
+    deg_fit = 2
     X = np.ones((len(x), deg_fit+1))
     for i in range(1, deg_fit+1):
         X[:,i] = x ** i
     X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.3)
-    dnn = NeuralNetwork(X_train,Y_train,batch_size=10,eta=0.001,lmbd=0.001,epochs=50,n_hidden_neurons=25)
-    dnn.train()
+    dnn = NeuralNetwork(X_train,Y_train,batch_size=100,eta=0.001,lmbd=0.0,epochs=1000,n_hidden_neurons=50)
+    dnn.train(show=True)
     pred_test, error_est_test = dnn.predict(X_test,Y_test)
     pred_train, error_est_train = dnn.predict(X_train, Y_train)
     # print("Accuracy score on training data: " + str(accuracy_score(dnn.predict(X_train[0], Y_train)[0], Y_train)))

project 2/Layer.py
@@ -17,7 +17,7 @@ class Layer():
             self,
             dimension_in,
             dimension_out,
-            act_func="relu",
+            act_func="sigmoid",
             input_layer=False,
             output_layer=False,
             ):
@@ -26,13 +26,10 @@ class Layer():
         self.dimension_out = dimension_out
         self.act_func = act_func
         self.output_layer = output_layer
+        self.input_layer = input_layer
 
-        if input_layer:             # Input layer does not change the input, just changes the dimensionality.
-            self.weights = np.eye(self.dimension_in, self.dimension_out)
-            self.bias = np.zeros((1, self.dimension_out))
-        else:
-            self.weights = np.random.randn(self.dimension_in, self.dimension_out)
-            self.bias = np.zeros((1, self.dimension_out)) + 0.01
+        self.weights = np.random.randn(self.dimension_in, self.dimension_out) / self.dimension_in
+        self.bias = np.zeros((1, self.dimension_out))
 
     def forward_prop(self, input_value):
         self.input = input_value
@@ -41,27 +38,30 @@ class Layer():
 
         return self.output
 
-    def backward_prop(self, error_prev_layer, lmbda):
+    def backward_prop(self, error_prev_layer, lmbda, weights_prev_layer = 0):
         if self.output_layer:
             self.grad_w = self.input.T @ error_prev_layer
-            self.grad_b = np.sum(error_prev_layer, axis=0)
+            self.grad_b = np.sum(error_prev_layer, axis=0, keepdims=True)
             if lmbda > 0.0:
                 self.grad_w += lmbda * self.weights
 
-            self.error_layer = error_prev_layer @ self.weights.T * self.grad_activation_function(self.output)
+            # self.error_layer = (error_prev_layer @ self.weights.T) * self.grad_activation_function(self.output)
 
-            return self.error_layer
+            return error_prev_layer
+
         else:
-            self.error_layer = error_prev_layer @ self.weights.T * self.grad_activation_function(self.output)
+            self.error_layer = (error_prev_layer @ weights_prev_layer.T) * self.grad_activation_function(self.output)
             self.grad_w = self.input.T @ self.error_layer
-            self.grad_b = np.sum(self.error_layer, axis=0)
+            self.grad_b = np.sum(self.error_layer, axis=0, keepdims=True)
         if lmbda > 0.0:
             self.grad_w += lmbda * self.weights
 
         # To avoid exploding gradients, we do gradient clipping
         # by removing a factor of 10 when the gradient exceed some threshold
-        self.grad_w = np.where(self.grad_w > 10, 0.1 * self.grad_w, self.grad_w)
-        self.grad_b[self.grad_b>10] = 0.1
+        # self.grad_w = np.where(self.grad_w > 100, 0.001 * self.grad_w, self.grad_w)
+        # self.grad_w = np.where(self.grad_w > 100, 0.1, self.grad_w)
+        #
+        # self.grad_b[self.grad_b>10] = 0.1
 
         return self.error_layer
 
@@ -83,4 +83,4 @@ class Layer():
             return np.ones_like(x)
 
         if self.act_func=="relu":
-            return np.where(x>0, 1, 0.01 * x) # Leaky ReLu
+            return np.where(x>0, 1, 0) # Leaky ReLu